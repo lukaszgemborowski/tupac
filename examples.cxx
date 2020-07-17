42d7b47a5bc3548573de32b8a0ceb1fcf1bb6a16
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2020-07-17 18:41:51 +0000

    doxygen documentation

--- examples.cxx
+++ examples.cxx
@@ -1,12 +1,16 @@
 #include "tupac.hpp"
+#include <iostream>
 
 struct aClass {};
 
 int main()
 {
-    auto t = std::make_tuple(1, 2, "foo", 3, 3.14, aClass{});
-    auto t2 = t | tupac::remove_if(tupac::is_integral || tupac::is_class);
-    static_assert(std::is_same_v<decltype(t2), std::tuple<const char *, double>>);
+    auto print = [](auto &e) { std::cout << e << ", "; };
+
+    std::make_tuple(aClass{}, 1)
+        | tupac::push_back(3.4)
+        | tupac::remove_if(tupac::is_integral || tupac::is_class)
+        | tupac::foreach(print);
 
     auto t3 = std::make_tuple(1, 2.3);
     auto t4 = t3 | tupac::mutate(tupac::make_reference);
--- include/tupac.hpp
+++ include/tupac.hpp
@@ -8,6 +8,7 @@
 # error "tupac requires at least C++17 support"
 #else
 
+/// \brief Main tupac namespace
 namespace tupac
 {
 namespace detail
@@ -48,15 +49,53 @@ constexpr auto tuple_from_indices(Tuple tuple, std::integer_sequence<std::size_t
 
 } // detail
 
-
+/// \brief Tuple algorithms.
+///
+/// This namespace contains all basic building blocks for a higher
+/// level "syntactic suger" features of this library. You may want
+/// to use functions from this namespace if you prefer.
 namespace algo
 {
+/// \defgroup algo Algorithms
+/// Basic tuple algorithms.
+/// \{
+
+/// \brief Execute a functor over each tuple element.
+///
+/// \param[in] tup input tuple
+/// \param[in] fun user provided functor
+///
+/// Invoke a functor passing a reference to each tuple element.
+/// Functor should accept a generic parameter type and/or
+/// handle each type contained in the tuple.
+/// \code{.cpp}
+/// auto tup = make_tuple(1, "foo");
+/// algo::for_each(
+///     tup,
+///     [](auto const& e) {
+///         cout << e << ", ";
+///     }
+/// );
+/// \endcode
 template<class Tuple, class Fun>
 void for_each(Tuple &tup, Fun fun)
 {
     std::apply([&](auto&... args) { (fun(args), ...); }, tup);
 }
 
+/// \brief Remove elements that satisfy predicate
+///
+/// \param[in] tup input tuple
+/// \param[in] fun user provided predicate
+///
+/// Creates a new tuple that does not contain elements
+/// matching provided predicate.
+/// \code{.cpp}
+/// auto t = std::make_tuple(1, 2.2, 3, "");
+/// auto t2= algo::remove_if(is_integral)
+/// static_assert(is_same_v<decltype(t2), std::tuple<double, const char*>>)
+/// \endcode
+/// \see pred
 template<class Tuple, class Fun>
 auto remove_if(Tuple const& tup, Fun fun)
 {
@@ -64,6 +103,23 @@ auto remove_if(Tuple const& tup, Fun fun)
     return detail::tuple_from_indices(tup, detail::make_index_sequence_of(tup, negate));
 }
 
+/// \brief Change type and/or value of each tuple element
+///
+/// \param[in] tup input tuple
+/// \param[in] fun mutating function
+///
+/// Translates every tuple element according to provided functor in
+/// such a way that new tuple element have type of decltype(fun(tuple_element))
+/// and a value of fun(tuple_element). A good example is to transform a tuple
+/// of integers into tuple of references to the original tuple elements.
+///
+/// \code{.cpp}
+/// auto t = std::make_tuple(1, 2.3);
+/// auto t2 = algo::mutate(t, make_reference);
+/// static_assert(is_same_v<decltype(t2), tuple<int &, double &>>);
+/// \endcode
+///
+/// \see make_reference
 template<class Tuple, class Fun>
 auto mutate(Tuple&& tup, Fun fun)
 {
@@ -78,6 +134,23 @@ auto mutate(Tuple&& tup, Fun fun)
     );
 }
 
+/// \brief Add an element at the end of the tuple
+///
+/// \param[in] tup input tuple
+/// \param[in] value value to append
+///
+/// Creates new tuple and append T at the end.
+template<class T, class... Args>
+auto push_back(std::tuple<Args...> const& tup, T&& value)
+{
+    return std::apply(
+        [&](auto&... args) { return std::tuple<Args..., T>{args..., value}; },
+        tup
+    );
+}
+
+/// \}
+
 } // algo
 
 namespace detail
@@ -85,7 +158,7 @@ namespace detail
 template<class Fun>
 struct foreach_t {
     template<class Tup>
-    auto operator()(Tup &tup) {
+    auto operator()(Tup&& tup) {
         algo::for_each(tup, fun_);
         return tup;
     }
@@ -96,11 +169,8 @@ struct foreach_t {
 template<class T>
 struct push_back_t {
     template<class... Args>
-    auto operator()(std::tuple<Args...> &tup) {
-        return std::apply(
-            [&, this](auto&... args) { return std::tuple<Args..., T>{args..., arg_}; },
-            tup
-        );
+    auto operator()(std::tuple<Args...> const& tup) {
+        return algo::push_back(tup, arg_);
     }
 
     T arg_;
@@ -169,37 +239,119 @@ auto operator || (std_trait_adapter<A>, std_trait_adapter<B>) {
 } // detail
 
 // operations
+
+/// \defgroup func Modifiers
+/// \{
+
+/// \brief Execute a functor over each tuple element.
+///
+/// Executes algo::for_each over a tuple.
+///
+/// \code{.cpp}
+/// a_tuple | foreach([](auto &e) { std::cout << e << ", "; });
+/// \endcode
 inline detail::binder_t<detail::foreach_t> foreach;
+
+/// \brief Add an element at the end of the tuple
+///
+/// Executes algo::push_back over a tuple.
+///
+/// \code{.cpp}
+/// auto t = std::make_tuple("foo");
+/// auto u = t | push_back(42);
+/// \endcode
 inline detail::binder_t<detail::push_back_t> push_back;
+
+/// \brief Remove elements that satisfy predicate
+///
+/// Executes algo::remove_if over a tuple.
+///
+/// \code{.cpp}
+/// auto t = std::make_tuple(1, 2.2, 3, "");
+/// auto t2 = t | remove_if(is_integral)
+/// static_assert(is_same_v<decltype(t2), std::tuple<double, const char*>>)
+/// \endcode
+/// \see pred
 inline detail::binder_t<detail::remove_if> remove_if;
+
+/// \brief Change type and/or value of each tuple element
+///
+/// Executes algo::mutate over a tuple.
+///
+/// \code{.cpp}
+/// auto t = std::make_tuple(1, 2.3);
+/// auto t2 = t | mutate(make_reference);
+/// static_assert(is_same_v<decltype(t2), tuple<int &, double &>>);
+/// \endcode
+///
+/// \see make_reference
 inline detail::binder_t<detail::mutate_t> mutate;
+/// \}
 
-// qualifiers
-#define ADAPT_STD(TRAIT) inline detail::std_trait_adapter<std::TRAIT> TRAIT;
-ADAPT_STD(is_integral);
-ADAPT_STD(is_floating_point);
-ADAPT_STD(is_array);
-ADAPT_STD(is_enum);
-ADAPT_STD(is_union);
-ADAPT_STD(is_class);
-ADAPT_STD(is_function);
-ADAPT_STD(is_pointer);
-ADAPT_STD(is_lvalue_reference);
-ADAPT_STD(is_rvalue_reference);
-ADAPT_STD(is_member_object_pointer);
-ADAPT_STD(is_member_function_pointer);
+/// \defgroup pred Predicates
+/// \{
 
-ADAPT_STD(is_fundamental);
-ADAPT_STD(is_arithmetic);
-ADAPT_STD(is_scalar);
-ADAPT_STD(is_object);
-ADAPT_STD(is_compound);
-ADAPT_STD(is_reference);
-ADAPT_STD(is_member_pointer);
+/// \brief Wraps std::is_integral trait
+inline detail::std_trait_adapter<std::is_integral> is_integral;
+
+/// \brief Wraps std::is_floating_point trait
+inline detail::std_trait_adapter<std::is_floating_point> is_floating_point;
+
+/// \brief Wraps std::is_array trait
+inline detail::std_trait_adapter<std::is_array> is_array;
+
+/// \brief Wraps std::is_enum trait
+inline detail::std_trait_adapter<std::is_enum> is_enum;
+
+/// \brief Wraps std::is_union trait
+inline detail::std_trait_adapter<std::is_union> is_union;
+
+/// \brief Wraps std::is_class trait
+inline detail::std_trait_adapter<std::is_class> is_class;
+
+/// \brief Wraps std::is_function trait
+inline detail::std_trait_adapter<std::is_function> is_function;
+
+/// \brief Wraps std::is_pointer trait
+inline detail::std_trait_adapter<std::is_pointer> is_pointer;
+
+/// \brief Wraps std::is_lvalue_reference trait
+inline detail::std_trait_adapter<std::is_lvalue_reference> is_lvalue_reference;
+
+/// \brief Wraps std::is_rvalue_reference trait
+inline detail::std_trait_adapter<std::is_rvalue_reference> is_rvalue_reference;
+
+/// \brief Wraps std::is_member_object_pointer trait
+inline detail::std_trait_adapter<std::is_member_object_pointer> is_member_object_pointer;
+
+/// \brief Wraps std::is_member_function_pointer trait
+inline detail::std_trait_adapter<std::is_member_function_pointer> is_member_function_pointer;
+
+/// \brief Wraps std::is_fundamental trait
+inline detail::std_trait_adapter<std::is_fundamental> is_fundamental;
+
+/// \brief Wraps std::is_arithmetic rait
+inline detail::std_trait_adapter<std::is_arithmetic> is_arithmetic;
+
+/// \brief Wraps std::is_scalar trait
+inline detail::std_trait_adapter<std::is_scalar> is_scalar;
+
+/// \brief Wraps std::is_object trait
+inline detail::std_trait_adapter<std::is_object> is_object;
+
+/// \brief Wraps std::is_compound trait
+inline detail::std_trait_adapter<std::is_compound> is_compound;
+
+/// \brief Wraps std::is_reference trait
+inline detail::std_trait_adapter<std::is_reference> is_reference;
+
+/// \brief Wraps std::is_member_pointer trait
+inline detail::std_trait_adapter<std::is_member_pointer> is_member_pointer;
+
+/// \}
 
 // TODO: type properties, supported operations
 // TODO: new adapter required: type relationships
-#undef ADAPT_STD
 
 // mutators
 struct make_reference_t {
